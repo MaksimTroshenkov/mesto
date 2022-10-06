22bae68fb0eb75f465e33a3bd3b7ee972f7a0840
Maksim Troshenkov <maksthemoscow@yandex.ru>
2022-10-06 23:27:21 +0400

    1

--- style.js
+++ style.js
@@ -1,8 +1,16 @@
-const openFormBtn = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
 const POPUP_ACTIVE_CLASS = "popup_active";
 
-openFormBtn.addEventListener("click", (event) => {
-  console.log(event);
+const openFormBtn = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const popupContainer = document.querySelector(".popup__container");
+const popupCloseBtn = document.querySelector(".popup__close");
+
+openFormBtn.addEventListener("click", () => {
   popup.classList.add(POPUP_ACTIVE_CLASS);
+});
+
+popup.addEventListener("click", (event) => {
+  if(!popupContainer.contains(event.target) || event.target === popupCloseBtn) {
+    popup.classList.remove(POPUP_ACTIVE_CLASS);
+  }
 });
