de1c6ea94ed57cdf544ceceb90b56cc5b07b2b13
Maksim Troshenkov <maksthemoscow@yandex.ru>
2022-10-07 19:19:50 +0400

    1

--- style.js
+++ style.js
@@ -5,12 +5,28 @@ const popup = document.querySelector(".popup");
 const popupContainer = document.querySelector(".popup__container");
 const popupCloseBtn = document.querySelector(".popup__close");
 
+const popupForm = document.querySelector(".popup__form");
+const submitBtn = document.querySelector(".popup__submit");
+const profileName = document.querySelector(".profile__name");
+const profileText = document.querySelector(".profile__text");
+const nameInput = document.querySelector(".popup__input_name");
+const textInput = document.querySelector(".popup__input_text");
+
 openFormBtn.addEventListener("click", () => {
   popup.classList.add(POPUP_ACTIVE_CLASS);
 });
 
 popup.addEventListener("click", (event) => {
-  if(!popupContainer.contains(event.target) || event.target === popupCloseBtn) {
+  if(!popupContainer.contains(event.target) || event.target === popupCloseBtn || event.target === submitBtn) {
     popup.classList.remove(POPUP_ACTIVE_CLASS);
   }
 });
+
+
+
+function addForm (evt) {
+  evt.preventDefault();
+  profileName.innerText = nameInput.value;
+  profileText.innerText = textInput.value;
+}
+popupForm.addEventListener("submit", addForm);
