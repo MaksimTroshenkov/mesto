910ce1b1cd16964bfc3718003d844eb5d9815596
Maksim Troshenkov <maksthemoscow@yandex.ru>
2022-10-23 00:55:44 +0400

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const POPUP_ACTIVE_CLASS = "popup_active";
 
 const openFormBtn = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
+const popup = document.querySelector(".popup_edit-form");
 const popupContainer = document.querySelector(".popup__container");
 const popupCloseBtn = document.querySelector(".popup__close-button");
 
@@ -34,3 +34,61 @@ function addForm (evt) {
   profileText.innerText = textInput.value;
 }
 popupForm.addEventListener("submit", addForm);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const list = document.querySelector(".element");
+const template = document.querySelector("#list-item-template").content.querySelector(".element__list");
+console.log(template);
+
+
+const cards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const createCards = (item) => {
+  const element = template.cloneNode(true);
+  list.append(element);
+
+  const cardsName = element.querySelector(".element__text");
+  cardsName.textContent = item.name;
+
+  const cardsUrl = element.querySelector(".element__image");
+  cardsUrl.src = item.link;
+};
+
+cards.forEach(createCards);
